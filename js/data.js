6e5becd6257f6fb8d298cf310b81ee78f4eba81f
0SU2 <or_zv18v@outlook.com>
2024-03-14 20:06:50 -0600

    fixing errors

--- js/data.js
+++ js/data.js
@@ -27,6 +27,36 @@ const songs = [
         name: 'Aruarian Dance',
         path: 'music/song5.mp3',
         artist: 'Nujabes',
-        cover: './images/disk5.jpg'
+        cover: './images/disk5.jpg',
+    },
+    {
+        name: 'Ghosts n Stuff',
+        path: 'music/song6.mp3',
+        artist: 'deadmau5',
+        cover: './images/disk6.jpg',
+    },
+    {
+        name: 'Always',
+        path: 'music/song7.mp3',
+        artist: 'keshi',
+        cover: './images/disk7.jpg',
+    },
+    {
+        name: 'EARFQUAKE',
+        path: 'music/song8.mp3',
+        artist: 'Tyler, The Creator',
+        cover: './images/disk8.jpg',
+    },
+    {
+        name: 'Doomsday',
+        path: 'music/song9.mp3',
+        artist: 'Tyler, The Creator',
+        cover: './images/disk9.jpg',
+    },
+    {
+        name: 'Something About Us',
+        path: 'music/song10.mp3',
+        artist: 'Daft Punk',
+        cover: './images/disk10.jpg',
     }
 ]
