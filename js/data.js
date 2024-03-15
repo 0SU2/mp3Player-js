52e069c994471b5ec34b8bc03e259301c781c696
0SU2 <or_zv18v@outlook.com>
2024-03-14 19:53:05 -0600

    first commit

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: ' Emancipator',
+        path: 'music/song1.mp3',
+        artist: 'Nujabes',
+        cover: './images/disk1.jpg'
+    },
+    {
+        name: 'Peanut Butter & Tears',
+        path: 'music/song2.mp3',
+        artist: 'DPR IAN',
+        cover: './images/disk2.jpg'
+    },
+    {
+        name: 'TikTok',
+        path: 'music/song3.mp3',
+        artist: 'Joji',
+        cover: './images/disk3.jpg'
+    },
+    {
+        name: 'Luv(sic)',
+        path: 'music/song4.mp3',
+        artist: 'Nujabes',
+        cover: './images/disk4.jpg'
+    },
+    {
+        name: 'Aruarian Dance',
+        path: 'music/song5.mp3',
+        artist: 'Nujabes',
+        cover: './images/disk5.jpg'
+    }
+]
